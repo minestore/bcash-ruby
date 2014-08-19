e0ff80443dd653384eea65dc725844479db7e2bc
Raphael Costa <raphael@raphaelcosta.net>
2014-08-19 12:40:05 -0300

    fix travis

--- lib/bcash/client.rb
+++ lib/bcash/client.rb
@@ -1,9 +1,13 @@
+require 'base64'
+
 module Bcash
   class Client
     include HTTParty
+
     attr_reader :token, :key, :email
 
     base_uri "https://api.bcash.com.br/service"
+    default_timeout 20
     format :json
 
     def initialize(opts={})
@@ -13,7 +17,19 @@ module Bcash
       @email = opts[:email]
     end
 
-    def create_account
+    def create_account(data)
+      json_post 'createAccount'
+    end
+
+    def json_post(method, options={}, &block)
+      options[:headers]["Authorization"] = authorization_key
+      self.class.post("/#{method}/json", options, block)
+    end
+
+    private
+
+    def authorization_key
+      "Basic #{Base64.encode64("#{email}:#{token}")}"
     end
   end
 end
